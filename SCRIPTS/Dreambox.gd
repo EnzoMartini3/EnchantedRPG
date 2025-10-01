db8e6ccc6e8e141f644cab838aaeb66b71973cca
Enzo Martini <enzomoraesmartini@gmail.com>
2025-09-30 21:40:33 -0300

    Setup inicial Dreamtent

--- SCRIPTS/Dreambox.gd
+++ SCRIPTS/Dreambox.gd
@@ -1,9 +1,9 @@
 extends Area2D
 
-export(String, FILE, "*.tscn") var houseScenePath: String = "res://Gimmicks/DreamHouse.tscn"
-export var exitSpawnPoint: String = "HouseExitSpawn"
-
+onready var exitPoint = $ExitPoint
+export(String, FILE, "*.tscn") var houseScenePath: String = "res://Buildings/Dream Tent/DreamHouse.tscn"
 
 func _on_Dreambox_area_entered(area):
+	print("e")
 	var worldRoot = get_tree().get_root().find_node("World")
-	worldRoot.goToScene(houseScenePath, exitSpawnPoint)
+	worldRoot.goToScene(houseScenePath, exitPoint)
